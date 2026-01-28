c57953dd3104cc32e57eed0fb3ab38e620ddf526
ScriptTiger <thescripttiger@gmail.com>
2026-01-28 22:33:58 +0800

    Encode salt as base64

--- src/Crypto.go
+++ src/Crypto.go
@@ -12,13 +12,13 @@ import (
 func generateSalt() (string) {
 	salt := jsGo.Uint8Array.New(16)
 	jsGo.Crypto.Call("getRandomValues", salt)
-	return salt.Call("toHex").String()
+	return salt.Call("toBase64").String()
 }
 
 // Hash the password and a salt together, returning a promise which will resolve to the hash
 func argon2(salt, password string) (promise js.Value) {
 	return jsGo.Get("argon2").Call("hash", map[string]any{
 		"pass": password,
-		"salt": jsGo.Uint8Array.Call("fromHex", salt).String(),
+		"salt": jsGo.Uint8Array.Call("fromBase64", salt).String(),
 	})
 }
